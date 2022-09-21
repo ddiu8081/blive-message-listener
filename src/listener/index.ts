eb904ef927c3072900f8264303af8ea09db3bf6c
ddiu8081 <ddiu8081@gmail.com>
2022-09-22 02:47:41 +0800

    feat: add SUPER_CHAT_MESSAGE, WATCHED_CHANGE listener

--- src/listener/index.ts
+++ src/listener/index.ts
@@ -2,18 +2,23 @@ import {
   DANMU_MSG, type DanmuMsgHandler,
   GUARD_BUY, type GuardBuyHandler,
   SEND_GIFT, type GiftHandler,
+  SUPER_CHAT_MESSAGE, type SuperChatHandler,
+  WATCHED_CHANGE, type WatchedChangeHandler,
 } from '../parser'
-import type { Danmu, BaseMsg } from '../app'
+import type { Danmu } from '../app'
 import type { KeepLiveTCP } from 'bilibili-live-ws'
 
 export type MsgHandler = Partial<
   & DanmuMsgHandler
   & GuardBuyHandler
   & GiftHandler
+  & SuperChatHandler
+  & WatchedChangeHandler
 >
 
-const normalizeDanmu = <T extends BaseMsg>(msgType: string, data: T): Danmu<T> => {
+const normalizeDanmu = <T>(msgType: string, data: T): Danmu<T> => {
   const timestamp = Date.now()
+  // @ts-ignore
   const id = `${timestamp}_${msgType}_${data.user?.uid}`
   return {
     id,
@@ -47,6 +52,22 @@ export const listenAll = (instance: KeepLiveTCP, handler?: MsgHandler) => {
     instance.on(SEND_GIFT.eventName, (data: any) => {
       const parsedData = SEND_GIFT.parser(data)
       handler[SEND_GIFT.handlerName]?.(normalizeDanmu(SEND_GIFT.eventName, parsedData))
+    })
+  }
+
+  // SUPER_CHAT_MESSAGE
+  if (handler[SUPER_CHAT_MESSAGE.handlerName]) {
+    instance.on(SUPER_CHAT_MESSAGE.eventName, (data: any) => {
+      const parsedData = SUPER_CHAT_MESSAGE.parser(data)
+      handler[SUPER_CHAT_MESSAGE.handlerName]?.(normalizeDanmu(SUPER_CHAT_MESSAGE.eventName, parsedData))
+    })
+  }
+
+  // WATCHED_CHANGE
+  if (handler[WATCHED_CHANGE.handlerName]) {
+    instance.on(WATCHED_CHANGE.eventName, (data: any) => {
+      const parsedData = WATCHED_CHANGE.parser(data)
+      handler[WATCHED_CHANGE.handlerName]?.(normalizeDanmu(WATCHED_CHANGE.eventName, parsedData))
     })
   }
 }
