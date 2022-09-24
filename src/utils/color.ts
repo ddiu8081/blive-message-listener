a4b0fedb8a76c1e2cfe8b098c695510c063ceaab
ddiu8081 <ddiu8081@gmail.com>
2022-09-24 23:45:07 +0800

    fix: color parser utils

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -1,4 +1,4 @@
 export const intToColorHex = (int: number) => {
   const hex = int.toString(16)
-  return hex.length === 1 ? `#0${hex}` : `#${hex}`
+  return hex.length === 5 ? `#0${hex}` : `#${hex}`
 }
